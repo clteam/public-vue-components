962c2b214cb5cf9b156f642016a21f20235529c0
dunizb <dunizb@outlook.com>
2017-11-24 21:58:12 +0800

    Update message
    调整默认高度

--- src/message/message.vue
+++ src/message/message.vue
@@ -52,7 +52,7 @@
     transform: translate(-50%, -50%);
     color: #fff;
     background-color: rgba(0, 0, 0, .7);
-    padding: 5px 10px;
+    padding: 10px 10px;
     font-size: 12px;
     border-radius: 3px;
     -webkit-border-radius: 3px;
